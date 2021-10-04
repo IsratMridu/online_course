89240b43451226c5813114e20157ac44d823fa23
shams-jubair <2017-1-60-032@std.ewubd.edu>
2021-10-04 08:23:12 +0600

    footer completed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,10 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearchLocation } from '@fortawesome/free-solid-svg-icons'
 import { faPhone } from '@fortawesome/free-solid-svg-icons'
 import { faMailBulk } from '@fortawesome/free-solid-svg-icons'
-import { NavLink } from 'react-router-dom';
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 const email = <FontAwesomeIcon icon={faMailBulk} />
 const phone = <FontAwesomeIcon icon={faPhone} />
+const arrowRight  = <FontAwesomeIcon icon={faArrowCircleRight} />
 const locationSearch = <FontAwesomeIcon icon={faSearchLocation} />
 
 
@@ -17,7 +18,7 @@ const Footer = () => {
         <div className="bg-color">
             <div className="container text-center">
             <div className="row">
-                <div className="col-sm-12 col-md-6 col-lg-6 py-5 mt-5">
+                <div className="col-sm-12 col-md-6 col-lg-4 py-5 mt-5">
                         <img src={logo} alt="uinversitImage" className="img-fluid bg-white p-2 rounded-3" />
                         <h1 className="text-bold text-white mt-3">Department Office</h1>
                         <h3 className="text-white">{locationSearch}  A/2, Jahurul Islam Avenue</h3>
@@ -31,11 +32,19 @@ const Footer = () => {
 
                 
 
-                <div className="col-sm-12 col-md-6 col-lg-6 text-white py-5">
+                <div className="col-sm-12 col-md-6 col-lg-4 text-white py-5">
                         <h1>Location Map</h1>
                         <div>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.4107330485617!2d90.42337781475248!3d23.76838398458049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78bc678693d%3A0x3e87f7b866c0e38!2sEast%20West%20University!5e0!3m2!1sen!2sbd!4v1633293665612!5m2!1sen!2sbd" width="600" height="450" style={{border:"0"}} allowfullscreen="" loading="lazy" title="googleMap"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3651.4107330485617!2d90.42337781475248!3d23.76838398458049!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c78bc678693d%3A0x3e87f7b866c0e38!2sEast%20West%20University!5e0!3m2!1sen!2sbd!4v1633293665612!5m2!1sen!2sbd" width="320" height="500" style={{border:"0"}} allowFullScreen="" loading="lazy" title="googleMap"></iframe>
                         </div>
+                </div>
+                <div className="col-sm-12 col-md-6 col-lg-4 text-white py-5">
+                    <h1>Newsletter</h1>
+                    <p>Subscribe Our Newsletter to Get More Update and Join Our Course Information.</p>
+                    <div className="d-flex justify-content-center">
+                        <input placeholder="Enter Your Email" className="p-2 w-75"  />
+                        <button className="btn btn-warning">{arrowRight}</button>
+                    </div>
                 </div>
 
             </div>
